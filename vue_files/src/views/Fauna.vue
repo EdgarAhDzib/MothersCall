65cd00d9a1511c163908ff9cbcdc58aff96b39ec
Edgar Martin del Campo <edgarmdcesp@gmail.com>
2018-07-05 20:24:34 -0500

    New directory for Vue files

---
+++ vue_files/src/views/Fauna.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="fauna">
+    <h1>Fauna of Mother's Call</h1>
+    <div v-for="animal in fauna" :key="animal.id">
+		<h3>{{animal.name}} : <em>{{animal.indigenous}}</em></h3>
+    </div>
+  </div>
+</template>
+<script>
+	import axios from 'axios'
+	export default {
+		mounted(){
+			axios.get('http://127.0.0.1:8000/motherscnotes/fauna')
+			.then((response) => {
+				console.log(response);
+				this.fauna = response.data;
+			})
+		},
+		data() {
+			return {
+				fauna: []
+			}
+		}
+	}
+</script>

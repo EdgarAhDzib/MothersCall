6ece37c7d6a657786c499a99365f03c06eb8b8ec
Edgar Martin del Campo <edgarmdcesp@gmail.com>
2018-08-16 22:21:26 -0500

    Whitenoise and requirements.txt

--- mothercdata/mothercdata/settings.py
+++ mothercdata/mothercdata/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ho)ocil-uf*im&6j596++zu4!-ve-$v6y%&rl)xxa)*mj9z)qq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['motherscall.herokuapp.com']
 #'heroku.com', 'motherscall.herokuapp.com'
 
 # Application definition
